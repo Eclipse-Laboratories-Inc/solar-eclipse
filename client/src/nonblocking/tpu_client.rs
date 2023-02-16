c42d0c57254ff3bb1b86103d97966f43fd2ac69d
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2023-02-16 00:02:20 -0800

    tpu-client: Increase expired blockhash retries to 10

--- client/src/nonblocking/tpu_client.rs
+++ client/src/nonblocking/tpu_client.rs
@@ -228,7 +228,7 @@ impl TpuClient {
         messages: &[Message],
         signers: &T,
     ) -> Result<Vec<Option<TransactionError>>> {
-        let mut expired_blockhash_retries = 5;
+        let mut expired_blockhash_retries = 10;
         let progress_bar = spinner::new_progress_bar();
         progress_bar.set_message("Setting up...");
 
