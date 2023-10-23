421012ef5ce55860b45fbda1a87125a8f6982080
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2023-10-23 16:00:17 +0400

    [test]: Failing test for Solar eclipse bls12-381 support
    Signed-off-by: Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>

---
+++ programs/bls12-381-tests/tests/process_transaction.rs
@@ -0,0 +1,31 @@
+use assert_matches::assert_matches;
+use rand::thread_rng;
+use solana_program_test::*;
+use solana_sdk::{
+    bls12_381_instruction::new_bls_12_381_instruction,
+    signature::Signer,
+    transaction::{Transaction, TransactionError},
+};
+
+
+#[tokio::test]
+async fn test_success() {
+    let mut context = ProgramTest::default().start_with_context().await;
+
+    let client = &mut context.banks_client;
+    let payer = &context.payer;
+    let recent_blockhash = context.last_blockhash;
+
+    let privkey = bls12_381_instruction::generate_key(&mut thread_rng());
+    let message_arr = b"hello";
+    let instruction = new_bls_12_381_instruction(&privkey, message_arr);
+
+    let transaction = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&payer.pubkey()),
+        &[payer],
+        recent_blockhash,
+    );
+
+    assert_matches!(client.process_transaction(transaction).await, Ok(()));
+}
