aa0ea1b3e444eb716df0823aa55b054daecfa6b5
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2023-10-23 19:16:51 +0400

    [feature]: Implement bls12-381 verify
    Signed-off-by: Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>

--- sdk/src/bls12_381_instruction.rs
+++ sdk/src/bls12_381_instruction.rs
@@ -1,11 +1,33 @@
-// use bls12_381::{G2Affine, G1Affine};
-
-use pair::{G2Affine,G1Affine, GroupOrderElement};
+/// This module is a port of Hyperledger Ursa's built-in BLS
+/// sig-verify. Only a single key verification is implemented at this
+/// point.
+use pair::{G1Affine, G2Affine, GroupOrderElement};
 pub const SECRET_KEY_LENGTH: usize = 32;
 
 pub mod pair {
-    use amcl::bn254::{ecp2::ECP2, ecp::ECP, big::BIG, rom::CURVE_ORDER};
-    use super::Error;
+    use {
+        super::Error,
+        amcl::bn254::{big::BIG, ecp::ECP, ecp2::ECP2, rom::CURVE_ORDER},
+    };
+
+    #[derive(Copy, Clone, PartialEq)]
+    pub struct Pair(pub(crate) amcl::bn254::fp12::FP12);
+
+    impl Pair {
+        /// e(PointG1, PointG2, PointG1_1, PointG2_1)
+        pub fn pair2(p: &G1Affine, q: &G2Affine, r: &G1Affine, s: &G2Affine) -> Self {
+            let mut result =
+                amcl::bn254::pair::fexp(&amcl::bn254::pair::ate2(&q.0, &p.0, &s.0, &r.0));
+            result.reduce();
+
+            Self(result)
+        }
+    }
+
+    pub fn hash<T: sha2::Digest>(message: &[u8], mut hasher: T) -> Result<G1Affine, Error> {
+        hasher.update(message);
+        G1Affine::from_hash(hasher.finalize().as_slice())
+    }
 
     #[derive(Clone, Copy, PartialEq, Eq)]
     pub struct GroupOrderElement {
@@ -16,7 +38,9 @@ pub mod pair {
         pub const BYTES_REPR_SIZE: usize = amcl::bn254::rom::MODBYTES;
 
         pub fn new() -> Result<Self, Error> {
-            Ok(GroupOrderElement { bignum: random_mod_order()?})
+            Ok(GroupOrderElement {
+                bignum: random_mod_order()?,
+            })
         }
 
         pub fn seeded(seed: &[u8]) -> Result<GroupOrderElement, Error> {
@@ -28,8 +52,8 @@ pub mod pair {
                 let mut rng = amcl::rand::RAND::new();
                 rng.clean();
                 rng.seed(seed.len(), seed);
-                Ok(GroupOrderElement{
-                    bignum: BIG::randomnum(&BIG::new_ints(&CURVE_ORDER), &mut rng)
+                Ok(GroupOrderElement {
+                    bignum: BIG::randomnum(&BIG::new_ints(&CURVE_ORDER), &mut rng),
                 })
             }
         }
@@ -48,12 +72,13 @@ pub mod pair {
             } else {
                 let mut vec = bytes.to_vec(); // TODO: Remove allocation
                 let len = vec.len();
-                if len < amcl::bn254::rom::MODBYTES { // TODO: No-op?
+                if len < amcl::bn254::rom::MODBYTES {
+                    // TODO: No-op?
                     let diff = amcl::bn254::rom::MODBYTES - len;
                     let mut result = vec![0; diff];
                     result.append(&mut vec);
                     return Ok(GroupOrderElement {
-                        bignum: BIG::frombytes(&result)
+                        bignum: BIG::frombytes(&result),
                     });
                 } else {
                     Ok(GroupOrderElement {
@@ -79,10 +104,10 @@ pub mod pair {
     }
 
     #[derive(Debug, Clone, Copy)]
-    pub struct G2Affine(ECP2);
+    pub struct G2Affine(pub(crate) ECP2);
 
     #[derive(Debug, Clone, Copy)]
-    pub struct G1Affine(ECP);
+    pub struct G1Affine(pub(crate) ECP);
 
     impl G1Affine {
         pub fn from_hash(hash_bytes: &[u8]) -> Result<Self, super::Error> {
@@ -97,16 +122,17 @@ pub mod pair {
             Ok(G1Affine(point))
         }
 
-        pub fn to_bytes(self) -> Vec<u8> {
-            todo!()
-        }
-
         pub fn mul(&self, element: &GroupOrderElement) -> Result<G1Affine, Error> {
-            let r = self.0;
-            let mut bn = element.bignum;
-            Ok(Self (
-                amcl::bn254::pair::g1mul(&r, &mut bn),
-            ))
+            let mut bn: BIG = element.bignum;
+            Ok(Self(amcl::bn254::pair::g1mul(&self.0, &mut bn)))
+        }
+    }
+
+    impl G2Affine {
+        pub fn neg(self) -> Self {
+            let mut r = self.0;
+            r.neg();
+            Self(r)
         }
     }
 }
@@ -116,8 +142,8 @@ pub enum Error {
 }
 
 pub struct VerKey {
-    point: G2Affine,
-    bytes: Vec<u8>,             // TODO: avoid allocation?
+    pub(crate) point: G2Affine,
+    // bytes: Vec<u8>,             // TODO: avoid allocation?
 }
 
 pub struct SignKey {
@@ -127,13 +153,19 @@ pub struct SignKey {
 
 impl From<GroupOrderElement> for SignKey {
     fn from(group_order_element: GroupOrderElement) -> Self {
-        Self {group_order_element, bytes: group_order_element.to_bytes()}
+        Self {
+            group_order_element,
+            bytes: group_order_element.to_bytes(),
+        }
     }
 }
 
 impl SignKey {
     pub fn new(seed: Option<&[u8]>) -> Result<SignKey, Error> {
-        Ok(seed.map(GroupOrderElement::seeded).unwrap_or_else(GroupOrderElement::new)?.into())
+        Ok(seed
+            .map(GroupOrderElement::seeded)
+            .unwrap_or_else(GroupOrderElement::new)?
+            .into())
     }
 
     pub fn as_bytes(&self) -> &[u8] {
@@ -143,39 +175,51 @@ impl SignKey {
     pub fn sign(&self, message: &[u8]) -> Result<Signature, Error> {
         let point = {
             let hasher = sha2::Sha256::default();
-            hash(message, hasher)?.mul(&self.group_order_element)
+            pair::hash(message, hasher)?.mul(&self.group_order_element)
         }?;
 
-        Ok(Signature {
-            point,
-            bytes: point.to_bytes(),
-        })
+        Ok(Signature { point })
     }
 }
 
-pub fn hash<T: sha2::Digest>(message: &[u8], mut hasher: T) -> Result<G1Affine, Error> {
-    hasher.update(message);
-    G1Affine::from_hash(hasher.finalize().as_slice())
-}
-
-
 #[derive(Debug)]
 pub struct Signature {
-    point: G1Affine,
-    bytes: Vec<u8>,
+    pub(crate) point: G1Affine,
+    // bytes: Vec<u8>,
+}
+
+pub struct Generator {
+    point: G2Affine,
 }
 
 impl Signature {
+    /// Verify the BLS12-381 signature provided the following data:
+    ///
+    /// # Arguments
+    /// `message` - the message that had been signed, represented as a slice of bytes.
+    /// `verification_key` - the `public_key` equivalent for the BLS12-381 signature scheme.
+    /// `generator` - the generator that was used to generate the signing key.
+    ///
+    /// It is assumed that the `message`, and `generator` are both
+    /// known to all parties.
     pub fn verify(
         &self,
         message: &[u8],
         verification_key: &VerKey,
-        // gen: Generator
+        generator: Generator,
     ) -> Result<bool, Error> {
-        todo!()
+        let hashpoint = pair::hash(message, sha2::Sha256::default())?;
+        Ok(amcl::bn254::fp12::FP12::isunity(
+            &pair::Pair::pair2(
+                &self.point,
+                &generator.point.neg(),
+                &hashpoint,
+                &verification_key.point,
+            )
+            .0,
+        ))
     }
 }
-
 
 pub struct KeyPair {
     pub sign: SignKey,
