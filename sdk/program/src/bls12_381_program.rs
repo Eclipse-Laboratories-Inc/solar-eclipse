3ee95ced88a59e8c5b12809359c6ca6ecb34f84a
Aleksandr Petrosyan <ap886@cantab.ac.uk>
2023-11-10 20:06:35 +0400

    [feature]: Implement solana native program interface to allow usage.
    Signed-off-by: Aleksandr Petrosyan <ap886@cantab.ac.uk>

---
+++ sdk/program/src/bls12_381_program.rs
@@ -0,0 +1,5 @@
+//! The [bls12-381 native program][np].
+//!
+//! [np]: https://docs.solana.com/developing/runtime-facilities/programs#ed25519-program
+
+crate::declare_id!("BLS12381SigVerify11111111111111111111111111");
